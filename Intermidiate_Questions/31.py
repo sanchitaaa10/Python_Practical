a83b144282abedd3401f08442b585f3e1ef78c43
sanchitaaa10 <2024.sanchitas@isu.ac.in>
2025-01-15 17:04:02 +0530

    Update 31.py

--- Intermidiate_Questions/31.py
+++ Intermidiate_Questions/31.py
@@ -1,8 +1,7 @@
-def factorial(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
+num= int(input("enter a number"))
 
-num = int(input("Enter a number: "))
-print(f"Factorial of {num} is {factorial(num)}")
+factorial=1
+for i in range(1,num+1):
+    factorial *= i
+print("factorial of ",num," is ",factorial)
+
