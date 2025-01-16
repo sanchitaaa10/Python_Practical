43f37ea472c07906920f03e178940be853ad8abc
sanchitaaa10 <2024.sanchitas@isu.ac.in>
2025-01-16 22:45:29 +0530

    Update 22.py

--- Basic _Questions/22.py
+++ Basic _Questions/22.py
@@ -1,4 +1,12 @@
-num_list = [int(x) for x in input("Enter numbers separated by space: ").split()]
-for i in range(len(num_list) // 2):
-    num_list[i], num_list[-i-1] = num_list[-i-1], num_list[i]
+input_numbers = input("Enter numbers separated by space: ")
+num_list = [int(x) for x in input_numbers.split()]
+
+length = len(num_list)
+half_length = length // 2
+
+for i in range(half_length):
+    temp = num_list[i]
+    num_list[i] = num_list[-i-1]
+    num_list[-i-1] = temp
+
 print("Reversed List:", num_list)
