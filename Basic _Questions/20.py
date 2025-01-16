4a38e56ed277e166c03d9bb59642c1c0dfe239f2
sanchitaaa10 <2024.sanchitas@isu.ac.in>
2025-01-16 22:43:21 +0530

    Update 20.py

--- Basic _Questions/20.py
+++ Basic _Questions/20.py
@@ -1,5 +1,6 @@
-num_list = list(map(int, input("Enter numbers separated by spaces: ").split()))
+input_numbers = input("Enter numbers separated by spaces: ")
+num_list = list(map(int, input_numbers.split()))
 
 list_sum = sum(num_list)
 
-print("Sum of the list is: ", list_sum)
+print("Sum of the list is:", list_sum)
