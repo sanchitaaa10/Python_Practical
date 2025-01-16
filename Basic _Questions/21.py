7de55aac63cf6d227d7490b609bad2dbfeaab555
sanchitaaa10 <2024.sanchitas@isu.ac.in>
2025-01-16 22:44:29 +0530

    Update 21.py

--- Basic _Questions/21.py
+++ Basic _Questions/21.py
@@ -1,9 +1,14 @@
-num_list = [int(x) for x in input("Enter numbers separated by space: ").split()]
-min_value, max_value = num_list[0], num_list[0]
+input_numbers = input("Enter numbers separated by space: ")
+num_list = [int(x) for x in input_numbers.split()]
+
+min_value = num_list[0]
+max_value = num_list[0]
+
 for num in num_list:
     if num < min_value:
         min_value = num
     if num > max_value:
         max_value = num
+
 print("Minimum:", min_value)
 print("Maximum:", max_value)
