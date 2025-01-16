cca0d638ee55a441a68a7abc975e876b4cc8de8d
sanchitaaa10 <2024.sanchitas@isu.ac.in>
2025-01-16 22:47:36 +0530

    Update 23.py

--- Basic _Questions/23.py
+++ Basic _Questions/23.py
@@ -1,7 +1,11 @@
-num_list = [int(x) for x in input("Enter numbers separated by space: ").split()]
+input_numbers = input("Enter numbers separated by space: ")
+num_list = [int(x) for x in input_numbers.split()]
+
 search_num = int(input("Enter number to count: "))
+
 count = 0
 for num in num_list:
     if num == search_num:
         count += 1
-print(f"{search_num} appears {count} times.")
+
+print(search_num," appears ",count, "times.")
