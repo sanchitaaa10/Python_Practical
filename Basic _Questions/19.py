a3c6d0eadec133d2e02ea7c0ccbc449828490533
sanchitaaa10 <2024.sanchitas@isu.ac.in>
2025-01-16 22:41:55 +0530

    Update 19.py

--- Basic _Questions/19.py
+++ Basic _Questions/19.py
@@ -1,3 +1,7 @@
-numbers = [int(input(f"Enter number {i+1}: ")) for i in range(5)]
+numbers = []
+for i in range(5):
+    num = int(input(f"Enter number {i+1}: "))
+    numbers.append(num)
+
 print("List:", numbers)
 print("Length of list:", len(numbers))
